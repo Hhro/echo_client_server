67ea5f7a897e0ff88017dfc03121c8694d41f935
Hhro <kwonsw212@gmail.com>
2019-12-04 16:22:41 +0900

    refactoring

--- lib/server.cpp
+++ lib/server.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-void Socket::set_addr(struct sockaddr_in & addr){
-    m_addr.sin_family = addr.sin_family;
-    m_addr.sin_port = addr.sin_port;
-    m_addr.sin_addr.s_addr = addr.sin_addr.s_addr;
-}
-
-bool Socket::set_nonblock_mode(){
-    int flags;
-
-    flags = fcntl(m_fd, F_GETFL);
-    if(flags<0)
-        return false;
-
-    flags |= O_NONBLOCK;
-
-    if(fcntl(m_fd, F_SETFL, flags) < 0)
-        return false;
-    
-    return true;
-}
-
 Server::Server(int port, int mode){
     int reuseaddr_on = 1;
     socklen_t addr_len = sizeof(sockaddr);
@@ -50,13 +29,6 @@ Server::Server(int port, int mode){
         error(1, "Failed to Listen on server socket.");
 }
 
-void Server::run_server(){
-    std::thread t1 = std::thread(&Server::msg_handler, this);
-    event_set(&m_ev_accept, m_fd, EV_READ|EV_PERSIST, invoke_cb_accept, this);
-    event_add(&m_ev_accept, NULL);
-    event_dispatch();
-    t1.join();
-}
 
 void Server::invoke_cb_accept(int fd, short ev, void *ctx){
     return (static_cast<Server*>(ctx))->cb_accept(fd);
@@ -68,6 +40,7 @@ void Server::invoke_client_handler(int fd, short ev, void *ctx){
 
 void Server::cb_accept(int fd){
     int client_fd;
+    char ip[INET_ADDRSTRLEN];
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(sockaddr);
     struct event *client_evt = (struct event*)calloc(sizeof(struct event), 1);
@@ -91,7 +64,6 @@ void Server::cb_accept(int fd){
     event_set(client_evt, client_fd, EV_READ|EV_PERSIST, invoke_client_handler, this);
     event_add(client_evt, NULL);
 
-    char ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, ip, INET_ADDRSTRLEN);
 
     std::cout << "Client from " << ip << "connected" << endl;
@@ -110,7 +82,7 @@ void Server::client_handler(int fd){
         goto close_conn;
 	}
 	else if (len < 0) {
-        /* Unexpected socket error occur */
+        /* Unexpected socket error */
         warn("Unexpected socket error, shutdown connection");
         goto close_conn;
 	}
@@ -173,10 +145,21 @@ void Server::msg_handler(){
             write(to_fd, content, len);
         }
         m_client_mtx.unlock();
+
+        delete(msg);
     }
 }
 
-Client::Client(int fd, struct sockaddr_in & addr){
-    m_fd = fd;
-    m_addr = addr;
+void Server::run_server(){
+    for(int i=0;i<MSGWORKERN;i++){
+        m_msg_worker.push_back(std::thread(&Server::msg_handler, this));
+    }
+
+    event_set(&m_ev_accept, m_fd, EV_READ|EV_PERSIST, invoke_cb_accept, this);
+    event_add(&m_ev_accept, NULL);
+    event_dispatch();
+
+    for(int i=0;i<MSGWORKERN;i++){
+        m_msg_worker[i].join();
+    }
 }
